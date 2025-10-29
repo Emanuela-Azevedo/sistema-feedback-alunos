850429646372c3a652722d290b910379af48a568
Luciana Farias <luciana02332@gmail.com>
2025-10-29 18:33:51 -0300

    Projeto Inicial Backend

---
+++ feedback_alunos/src/main/java/com/projetoDac/feedback_alunos/FeedbackAlunosApplication.java
@@ -0,0 +1,13 @@
+package com.projetoDac.feedback_alunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackAlunosApplication.class, args);
+	}
+
+}
---
+++ feedback_alunos/src/test/java/com/projetoDac/feedback_alunos/FeedbackAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoDac.feedback_alunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
