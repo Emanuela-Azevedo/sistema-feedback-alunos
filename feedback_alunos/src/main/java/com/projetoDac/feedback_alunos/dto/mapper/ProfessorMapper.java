790bcfddca0629dd5e291949a8f1632776e2598a
EmanuelaAzevedo <manujaine.2003@gmail.com>
2025-11-21 16:19:33 -0300

    Adiciona mappers de Usuario, Aluno e Perfil e etc
    mappers

---
+++ feedback_alunos/src/main/java/com/projetoDac/feedback_alunos/dto/mapper/ProfessorMapper.java
@@ -0,0 +1,19 @@
+package com.projetoDac.feedback_alunos.dto.mapper;
+
+import com.projetoDac.feedback_alunos.dto.ProfessorCreateDTO;
+import com.projetoDac.feedback_alunos.dto.ProfessorResponseDTO;
+import com.projetoDac.feedback_alunos.model.Professor;
+import org.modelmapper.ModelMapper;
+
+public class ProfessorMapper {
+	private static final ModelMapper mapper = new ModelMapper();
+
+	public static Professor toEntity(ProfessorCreateDTO dto) {
+		return mapper.map(dto, Professor.class);
+	}
+
+	public static ProfessorResponseDTO toDto(Professor professor) {
+		return mapper.map(professor, ProfessorResponseDTO.class);
+	}
+
+}
